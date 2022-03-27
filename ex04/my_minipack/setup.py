59314aa7bd3c5ad13dd114b859f8113ead4567bb
Arthur LAINE <arlaine@student.42.fr>
2022-03-27 16:54:31 +0200

    added root elems

---
+++ ex04/my_minipack/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name='my_minipack',
+    version='1.0.0',
+    author='arlaine',
+    license='',
+    description='My minipack',
+    author_email='arlaine@student.42.fr',
+    packages=['progressbar', 'logger'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Students/Developers',
+        'Topic :: Testing/Education',
+        'License ::',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only'
+    ]
+)
