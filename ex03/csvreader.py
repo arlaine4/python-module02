5e1a3773e6a71d38578afa04e8d0b51b11fe1f88
Arthur LAINE <arlaine@student.42.fr>
2022-03-28 12:24:10 +0200

    some typos and pep8 fix

--- ex03/csvreader.py
+++ ex03/csvreader.py
@@ -1,4 +1,4 @@
-class CsvReader():
+class CsvReader:
     def __init__(self, filename=None, sep=',', header=False, skip_top=0, skip_bottom=0):
         if not filename:
             raise ValueError("must specify a file to read")
@@ -12,7 +12,7 @@ class CsvReader():
         self.skip_top = skip_top
         self.skip_bottom = skip_bottom
         self.file_obj = None
-        self.fulldata = []
+        self.full_data = []
 
     def __enter__(self):
         try:
@@ -21,44 +21,31 @@ class CsvReader():
             self.file_obj = None
             return None
         for line in self.file_obj:
-            self.fulldata.append(list(map(str.strip, line.split(self.sep))))
+            self.full_data.append(list(map(str.strip, line.split(self.sep))))
         base_length = None
-        for i, elem in enumerate(self.fulldata):
+        for i, elem in enumerate(self.full_data):
             if i == 0:
                 base_length = len(elem)
             elem = [e for e in elem if e]
             if i != 0 and len(elem) != base_length:
                 return None
         return self
-        """if all(len(elem) == len(self.fulldata[0]) for elem in self.fulldata):
-            return self
-        else:
-            return None"""
 
-    def __exit__(self, type, value, traceback):
-        self.fulldata = []
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.full_data = []
         if self.file_obj:
             self.file_obj.close()
 
     def getdata(self):
-        """ Retrieves the data/records from skip_top to skip bottom.
-            Returns:
-            nested list (list(list, list, ...)) representing the data.
-        """
         start = self.skip_top
-        end = len(self.fulldata) - self.skip_bottom
+        end = len(self.full_data) - self.skip_bottom
         if self.header:
-            return self.fulldata[ start + 1 : end ]
+            return self.full_data[start + 1: end]
         else:
-            return self.fulldata[ start : end ]
+            return self.full_data[start: end]
 
     def getheader(self):
-        """ Retrieves the header from csv file.
-            Returns:
-            list: representing the data (when self.header is True).
-            None: (when self.header is False).
-        """
         if self.header:
-            return self.fulldata[0]
+            return self.full_data[0]
         else:
             return None
